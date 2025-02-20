2ad206b704506368a1f9886ffc5c764037d0b8ac
Seth <sethbarrett01@gmail.com>
2025-02-19 20:42:08 -0500

    Minor updates to req & python file

--- xseciot_core/listenCSV.py
+++ xseciot_core/listenCSV.py
@@ -1,16 +1,17 @@
 from flask import Flask, request
 import pandas as pd
 import io
+from pprint import pprint
 
 app = Flask(__name__)
 
 @app.route('/', methods=['POST'])
 def receive_csv():
     try:
-        csv_data = request.data.decode('utf-8')  # Read the incoming CSV
+        csv_data = request.data.decode('utf-8') 
         df = pd.read_csv(io.StringIO(csv_data))
         print("Received CSV data:")
-        print(df)
+        pprint(df)
         return "CSV received", 200
     except Exception as e:
         return f"Error: {e}", 400
