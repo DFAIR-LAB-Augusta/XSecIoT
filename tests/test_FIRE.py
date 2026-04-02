bf3ecbdbde883e307ef3990d38ceb35714fadea3
pooltab <sebarrett@augusta.edu>
2026-04-02 14:47:12 -0400

    test: removed old (garbage) tests

--- tests/test_FIRE.py
+++ tests/test_FIRE.py
@@ -1,6 +1,6 @@
-def test_import_fire():
-    try:
-        from fire import main, models, preprocessing  # noqa: F401
-    except ImportError:
-        assert False, 'Failed to import fire modules'
-    assert True
+# def test_import_fire():
+#     try:
+#         from fire import main, models, preprocessing  # noqa: F401
+#     except ImportError:
+#         assert False, 'Failed to import fire modules'
+#     assert True
